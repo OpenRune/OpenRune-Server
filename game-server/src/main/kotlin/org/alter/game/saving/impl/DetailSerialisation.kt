42550aca33b79cb3d18a60c756ae5990b2f87ef8
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-12-22 02:20:44 +0000

    Update DetailSerialisation.kt

--- game-server/src/main/kotlin/org/alter/game/saving/impl/DetailSerialisation.kt
+++ game-server/src/main/kotlin/org/alter/game/saving/impl/DetailSerialisation.kt
@@ -13,11 +13,14 @@ class DetailSerialisation(override val name: String = "details") : DocumentHandl
         client.tile = Tile(doc["tile"] as List<Int>)
         client.privilege = client.world.privileges.get(doc.getString("privilege"))?: Privilege.DEFAULT
         client.runEnergy = doc.getDouble("runEnergy") ?: 10000.00
+        client.gameframeTopLevel = doc.getString("display") ?: "interfaces.toplevel"
+        client.gameframeTopLevelLastKnown = doc.getString("display") ?: "interfaces.toplevel"
     }
 
     override fun asDocument(client: Client): Document = Document()
         .append("tile", listOf(client.tile.x, client.tile.z, client.tile.height))
         .append("privilege", client.privilege.name.uppercase())
         .append("runEnergy", client.runEnergy)
+        .append("display", client.gameframeTopLevel)
 
 }
