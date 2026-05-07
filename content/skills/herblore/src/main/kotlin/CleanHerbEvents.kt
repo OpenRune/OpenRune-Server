25538b39920d5c29a4332a0598053c076312c94a
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2026-05-07 22:44:08 +0200

    auto cleaning

--- content/skills/herblore/src/main/kotlin/CleanHerbEvents.kt
+++ content/skills/herblore/src/main/kotlin/CleanHerbEvents.kt
@@ -4,6 +4,7 @@ import org.rsmod.api.player.output.mes
 import org.rsmod.api.player.protect.ProtectedAccess
 import org.rsmod.api.player.stat.herbloreLvl
 import org.rsmod.api.script.onOpHeld1
+import org.rsmod.api.script.onPlayerQueueWithArgs
 import org.rsmod.api.table.herblore.HerbloreHerbsRow
 import org.rsmod.plugin.scripts.PluginScript
 import org.rsmod.plugin.scripts.ScriptContext
@@ -12,21 +13,35 @@ class CleanHerbEvents : PluginScript() {
 
     override fun ScriptContext.startup() {
         HerbloreHerbsRow.all().forEach { herb ->
-            onOpHeld1(herb.grimy) { clean(herb) }
+            onOpHeld1(herb.grimy) { startClean(herb) }
+        }
+        onPlayerQueueWithArgs("queue.herblore_clean") { continueClean(it.args) }
+    }
+
+    private fun ProtectedAccess.startClean(herb: HerbloreHerbsRow) {
+        if (!cleanOne(herb)) return
+        if (inv.contains(herb.grimy.internalName)) {
+            weakQueue("queue.herblore_clean", 6, herb)
         }
     }
 
-    private fun ProtectedAccess.clean(herb: HerbloreHerbsRow) {
+    private fun ProtectedAccess.continueClean(herb: HerbloreHerbsRow) {
+        if (!cleanOne(herb)) return
+        if (inv.contains(herb.grimy.internalName)) {
+            weakQueue("queue.herblore_clean", 6, herb)
+        }
+    }
+
+    private fun ProtectedAccess.cleanOne(herb: HerbloreHerbsRow): Boolean {
         if (player.herbloreLvl < herb.level) {
             player.mes("You need a Herblore level of ${herb.level} to clean this herb.")
-            return
+            return false
         }
-
         if (!invReplace(inv, herb.grimy.internalName, 1, herb.clean.internalName).success) {
-            return
+            return false
         }
-
         statAdvance("stat.herblore", herb.xp / 10.0)
         player.mes("You clean the ${herb.clean.name}.")
+        return true
     }
 }
