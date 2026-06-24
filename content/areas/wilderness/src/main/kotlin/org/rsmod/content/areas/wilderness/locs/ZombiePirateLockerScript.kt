6b585637267e85ce8bad78b83d5fdb9cb0b59e2b
a-thoren <alexander.thoren1+1@gmail.com>
2026-06-24 19:50:27 +0200

    feat: add unlocking of blip blocking scroll

--- content/areas/wilderness/src/main/kotlin/org/rsmod/content/areas/wilderness/locs/ZombiePirateLockerScript.kt
+++ content/areas/wilderness/src/main/kotlin/org/rsmod/content/areas/wilderness/locs/ZombiePirateLockerScript.kt
@@ -12,14 +12,16 @@ import org.rsmod.api.player.protect.ProtectedAccess
 import org.rsmod.api.player.vars.VarPlayerIntMapSetter
 import org.rsmod.api.repo.loc.LocRepository
 import org.rsmod.api.repo.obj.ObjRepository
+import org.rsmod.api.script.onOpHeld1
 import org.rsmod.api.script.onOpLoc1
-import org.rsmod.api.script.onOpLoc2
 import org.rsmod.game.inv.isType
 import org.rsmod.game.loc.BoundLocInfo
 import org.rsmod.plugin.scripts.PluginScript
 import org.rsmod.plugin.scripts.ScriptContext
 
 private const val ZOMBIE_PIRATE_KEY = "obj.zombie_pirate_wildy_key"
+private const val SCROLL = "obj.wilderness_blip_blocking_scroll"
+private const val VARBIT_SCROLL = "varbit.blip_blocking_scroll_used"
 
 private const val LOCKER_CLOSED = "loc.wildy_pirate_boat_locker"
 private const val LOCKER_OPEN = "loc.wildy_pirate_boat_locker_open"
@@ -36,6 +38,7 @@ constructor(
 
     override fun ScriptContext.startup() {
         onOpLoc1(LOCKER_CLOSED) { openLocker(it.loc) }
+        onOpHeld1(SCROLL) { unlockBlipScroll() }
     }
 
     private suspend fun ProtectedAccess.openLocker(loc: BoundLocInfo) {
@@ -70,4 +73,32 @@ constructor(
         }
         invAddOrDrop(objRepo, obj, drop.rollCount(random))
     }
+
+    private suspend fun ProtectedAccess.unlockBlipScroll() {
+        if (player.vars[VARBIT_SCROLL] == 1) {
+            objbox(SCROLL, "You can make out some faded words on the ancient parchment, but there's nothing more for you to learn.")
+            return
+        }
+
+        objbox(
+            SCROLL,
+            "You can make out some faded words on the ancient parchment, revealing the secrets of " +
+                "teleport negation. Would you like to absorb their power?",
+        )
+
+        val proceed =
+            choice2(
+                "Become immune to the Abyssal demon and <br>Elder Chaos druid teleportation attacks.",
+                true,
+                "Cancel",
+                false,
+                title = "This will consume the scroll."
+            )
+        if (!proceed) return
+
+        invDel(inv, SCROLL)
+        objbox(SCROLL, "You study the scroll and become immune to the Abyssal demon and Elder Chaos druid teleportation attacks.")
+        VarPlayerIntMapSetter.set(player, VARBIT_SCROLL, 1)
+    }
+
 }
