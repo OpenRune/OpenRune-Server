99fd1754dedee7c8896eb12f38c79ec9bbe3b9d6
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2026-08-04 20:31:24 +0100

    Rune Mysteries (#153)
    * Added Rune Mysteries

--- content/quest/src/main/kotlin/org/rsmod/content/quest/manager/QuestModule.kt
+++ content/quest/src/main/kotlin/org/rsmod/content/quest/manager/QuestModule.kt
@@ -1,10 +1,12 @@
 package org.rsmod.content.quest.manager
 
+import org.rsmod.content.quest.area.lumbridge.RuneMysteriesQuest
 import org.rsmod.plugin.module.PluginModule
 
 public class QuestModule : PluginModule() {
     override fun bind() {
         bindInstance<QuestRequirementResolver>()
+        bindInstance<RuneMysteriesQuest>()
     }
 }
 
