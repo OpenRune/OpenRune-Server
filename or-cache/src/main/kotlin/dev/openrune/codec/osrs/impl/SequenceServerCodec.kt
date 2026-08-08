abcb8091cfc1efa732e1c766c7afeb0b16987478
advnau <55411296+advnau@users.noreply.github.com>
2026-08-08 14:39:18 -0400

    fix: was using wrong field from seqs for priority

--- or-cache/src/main/kotlin/dev/openrune/codec/osrs/impl/SequenceServerCodec.kt
+++ or-cache/src/main/kotlin/dev/openrune/codec/osrs/impl/SequenceServerCodec.kt
@@ -32,7 +32,7 @@ class SequenceServerCodec(val sequences: Map<Int, SequenceType>? = null) :
             totalDelay = seq.frameDelays!!.sum()
         }
         maxLoops = seq.maxLoops
-        priority = seq.priority
+        priority = seq.forcedPriority
     }
 
     private fun SequenceType.getSkeletalLength(): Int = rangeEnd - rangeBegin
