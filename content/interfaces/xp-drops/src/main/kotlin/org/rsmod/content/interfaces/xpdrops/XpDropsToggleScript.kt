b8a2916a109a3f8994ccac5f3383e33d51b9ba42
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2026-08-19 10:13:50 +0100

    Update XpDropsToggleScript.kt

--- content/interfaces/xp-drops/src/main/kotlin/org/rsmod/content/interfaces/xpdrops/XpDropsToggleScript.kt
+++ content/interfaces/xp-drops/src/main/kotlin/org/rsmod/content/interfaces/xpdrops/XpDropsToggleScript.kt
@@ -1,27 +1,40 @@
 package org.rsmod.content.interfaces.xpdrops
 
 import jakarta.inject.Inject
+import org.rsmod.annotations.InternalApi
+import org.rsmod.api.player.output.runClientScript
 import org.rsmod.api.player.protect.ProtectedAccess
+import org.rsmod.api.player.protect.ProtectedAccessLauncher
+import org.rsmod.api.player.ui.ifSetHide
 import org.rsmod.api.player.vars.boolVarBit
 import org.rsmod.api.script.onIfOverlayButton
+import org.rsmod.api.script.onPlayerLogin
+import org.rsmod.events.EventBus
 import org.rsmod.game.entity.Player
 import org.rsmod.plugin.scripts.PluginScript
 import org.rsmod.plugin.scripts.ScriptContext
 
-class XpDropsToggleScript @Inject constructor() : PluginScript() {
+class XpDropsToggleScript @Inject constructor(private val protectedAccess: ProtectedAccessLauncher) : PluginScript() {
     override fun ScriptContext.startup() {
         onIfOverlayButton("component.orbs:xp_drops") { toggleXpDrops() }
+        onPlayerLogin { updateXpState(player) }
     }
 
     private fun ProtectedAccess.toggleXpDrops() {
         val enabled = player.xpDropsEnabled
         player.xpDropsEnabled = !enabled
-        if (enabled) {
-            ifCloseSub("interface.xp_drops")
-        } else {
-            ifOpenOverlay("interface.xp_drops", "component.toplevel_osrs_stretch:xp_drops")
+        updateXpState(player)
+    }
+
+    @OptIn(InternalApi::class)
+    fun updateXpState(player: Player) {
+        protectedAccess.launchLenient(player) {
+            if (!player.xpDropsEnabled) {
+                ifCloseSub("interface.xp_drops")
+            } else {
+                ifOpenOverlay("interface.xp_drops", "component.toplevel_osrs_stretch:xp_drops")
+            }
         }
-        ifOpenOverlay("interface.orbs", "component.toplevel_osrs_stretch:orbs")
     }
 }
 
